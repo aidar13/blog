7b7df38e88fbeb16a6c976e6e54aae3118bff3fe
Aidar <kanybek@spark.kz>
2025-03-17 01:02:07 +0500

    added auth api

--- app/Module/Auth/DTO/ResetPasswordDTO.php
+++ app/Module/Auth/DTO/ResetPasswordDTO.php
@@ -9,11 +9,15 @@ use App\Module\Auth\Requests\ResetPasswordRequest;
 final class ResetPasswordDTO
 {
     public string $email;
+    public string $token;
+    public string $password;
 
     public static function fromRequest(ResetPasswordRequest $request): self
     {
-        $self        = new self();
-        $self->email = $request->get('email');
+        $self           = new self();
+        $self->email    = $request->get('email');
+        $self->token    = $request->get('token');
+        $self->password = $request->get('password');
 
         return $self;
     }
--- app/Module/Auth/Requests/ResetPasswordRequest.php
+++ app/Module/Auth/Requests/ResetPasswordRequest.php
@@ -9,9 +9,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 /**
  * @OA\Schema (
- *     required={"email"},
+ *     required={"email", "token", "password"},
  *
  *     @OA\Property(property="email", type="email", example="email", description="email"),
+ *     @OA\Property(property="token", type="string", example="token", description="token"),
+ *     @OA\Property(property="password", type="string", example="password", description="password"),
  * )
  */
 final class ResetPasswordRequest extends FormRequest
@@ -19,7 +21,9 @@ final class ResetPasswordRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email', 'exists:users,email'],
+            'email'    => ['required', 'email', 'exists:users,email'],
+            'token'    => ['required', 'string'],
+            'password' => ['required', 'string', 'min:8'],
         ];
     }
 
